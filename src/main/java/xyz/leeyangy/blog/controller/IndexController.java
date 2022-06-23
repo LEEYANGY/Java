9494057688da3957b89525c2ce3afd346fd6502c
LEEYANGY <1787294587@qq.com>
2022-06-24 01:06:57 +0800

    thymeleaf:use thymeleaf

---
+++ src/main/java/xyz/leeyangy/blog/controller/IndexController.java
@@ -0,0 +1,78 @@
+package xyz.leeyangy.blog.controller;
+
+/**
+ * @author LEEYANGY
+ * @date 2022/6/22 1:24
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IndexController {
+//    @GetMapping("/{id}/{name}")
+//    public String index(@PathVariable Integer id,@PathVariable String name) {
+//        System.out.println("--------- index ---------");
+//        return "error/error";
+//    }
+
+    @GetMapping("/")
+    public String index() {
+        System.out.println("--------- index ---------");
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String indexx() {
+        System.out.println("--------- index ---------");
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        System.out.println("--------- blog ---------");
+        return "blog";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        System.out.println("--------- about ---------");
+        return "about";
+    }
+
+    @GetMapping("/archives")
+    public String archives() {
+        System.out.println("--------- archives ---------");
+        return "archives";
+    }
+
+    @GetMapping("/tags")
+    public String tags() {
+        System.out.println("--------- tags ---------");
+        return "tags";
+    }
+
+    @GetMapping("/types")
+    public String types() {
+        System.out.println("--------- types ---------");
+        return "types";
+    }
+
+//    admin
+    @GetMapping("/admin")
+    public String admin() {
+        System.out.println("--------- admin/blogs ---------");
+        return "admin/blogs";
+    }
+
+    @GetMapping("/news")
+    public String news() {
+        System.out.println("--------- admin/blogs-input ---------");
+        return "admin/blogs-input";
+    }
+
+    @GetMapping("/version")
+    public String versionAbout() {
+        System.out.println("--------- version/versionAbout ---------");
+        return "version/versionAbout";
+    }
+}
