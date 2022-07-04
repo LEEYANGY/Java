83288c15d0845031da41f5992c3d318df44f252e
LEEYANGY <1787294587@qq.com>
2022-07-04 16:18:23 +0800

    type

---
+++ src/main/java/xyz/leeyangy/blog/service/TypeService.java
@@ -0,0 +1,24 @@
+package xyz.leeyangy.blog.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import xyz.leeyangy.blog.domain.Type;
+
+import java.util.List;
+
+public interface TypeService {
+
+    Type saveType(Type type);
+
+    Type getType(Long id);
+
+    Page<Type> listType(Pageable pageable);
+
+    List<Type> listType();
+
+    List<Type> listTypeTop(Integer size);
+
+    Type updateType(Long id, Type type);
+
+    void deleteType(Long id);
+}
