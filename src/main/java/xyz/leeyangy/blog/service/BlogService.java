8c4bd3fd619a9ace295b5dcad8cfe4edf93ebc42
LEEYANGY <1787294587@qq.com>
2022-07-04 16:21:35 +0800

    blog

---
+++ src/main/java/xyz/leeyangy/blog/service/BlogService.java
@@ -0,0 +1,42 @@
+package xyz.leeyangy.blog.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import xyz.leeyangy.blog.domain.Blog;
+import xyz.leeyangy.blog.domain.BlogQuery;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author LEEYANGY
+ * @date 2022/6/22 1:28
+ */
+
+public interface BlogService {
+    Blog getBlog(Long id);
+
+    Blog getAndConvert(Long id);
+
+    Page<Blog> listBlog(Pageable pageable, BlogQuery Blog);
+
+    Page<Blog> listBlog(Pageable pageable);
+
+    Page<Blog> listBlog(String query, Pageable pageable);
+
+    Page<Blog> listBlog(Long tagId, Pageable pageable);
+
+    List<Blog> listRecommendBlogTop(Integer size);
+
+    // 归档页
+    Map<String, List<Blog>> archiveBlog();
+
+    Long countBlog();
+
+    Blog saveBlog(Blog Blog);
+
+    Blog updateBlog(Long id, Blog Blog);
+
+    void deleteBlog(Long id);
+
+    void updateViews(Long id);
+}
