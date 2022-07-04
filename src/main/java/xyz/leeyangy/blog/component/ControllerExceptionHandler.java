084b6420cedf8ca05b09a95160fcfab9888bf920
LEEYANGY <1787294587@qq.com>
2022-07-04 16:20:57 +0800

    handle -->> component

--- src/main/java/xyz/leeyangy/blog/component/ControllerExceptionHandler.java
+++ src/main/java/xyz/leeyangy/blog/component/ControllerExceptionHandler.java
@@ -1,9 +1,8 @@
-package xyz.leeyangy.blog.handler;
+package xyz.leeyangy.blog.component;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
