0d09b958b2ac0d4ee1f2290a05e3e9069c2da7b2
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-22 00:30:47 +0800

    继承自mvc-01

---
+++ spring-mvc-02/src/main/java/xyz/leeyangy/controller/UserController.java
@@ -0,0 +1,19 @@
+package xyz.leeyangy.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Package: xyz.leeyangy.controller
+ * @Author: LEEYANGYANG
+ * @Create: 2022/10/21 23:26
+ * @Description:
+ */
+@Controller
+public class UserController {
+
+    /**
+     *
+     * 使用
+     *
+     */
+}
