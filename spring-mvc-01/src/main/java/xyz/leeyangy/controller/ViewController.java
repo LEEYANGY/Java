6b311750c3b1e1a23533910815abb37b350c84c4
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-10-21 23:25:57 +0800

    redirect

--- spring-mvc-01/src/main/java/xyz/leeyangy/controller/ViewController.java
+++ spring-mvc-01/src/main/java/xyz/leeyangy/controller/ViewController.java
@@ -23,4 +23,23 @@ public class ViewController {
         System.out.println("--- forward ---");
         return "forward:/testThymeleafView";
     }
+
+    @RequestMapping("/testRedirect")
+    public String testRedirect(){
+        System.out.println("--- Redirect ---");
+        return "redirect:/testThymeleafView";
+    }
+
+    /**
+     *
+     *  转发请求：（一次请求）
+     *          第一次由浏览器，第二次是发送在内部服务器 （转发后的请求还是同一个request）
+     *  重定向请求：
+     *          第一次由servlet，第二次访问重定向地址（两次请求对应两个request/两次请求）
+     *
+     *      web-inf下的资源具有安全性，不允许重定向访问，只能通过服务器内部访问，不能通过浏览器访问
+     *
+     *      转发不能跨域(只能访问服务器内部资源)，重定向可以跨域(通过浏览器可以访问任意网络资源)
+     *
+     */
 }
